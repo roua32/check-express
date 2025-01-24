7a5f40f6993a743bc175d649c8f29e8a91c59a56
Zeiri Roua <roua.zyr@gmail.com>
2025-01-24 13:51:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ function timeCheker(req, res, next) {
   const now = new Date();
   const day = now.getDay();
   const hour = now.getHours();
-  if ((day === 0 || (day >= 1 && day <= 5)) && hour >= 9 && hour < 21) {
+  if (day >= 1 && day <= 5 && hour >= 9 && hour < 17) {
     next();
   } else {
     res.sendFile(path.resolve("public", "error.html"));
